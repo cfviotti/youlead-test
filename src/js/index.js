82622949ebbf8404f86bce154b565a7b90ca78f3
Caio Viotti <caio.viotti@buser.com.br>
2022-12-30 18:08:37 -0300

    Improve form focus

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import openToast from './app-toast'
 
-const form = document.querySelector('#home_form')
 const ctaButton = document.querySelector('[data-end-cta]')
+const formContainer = document.querySelector('[data-form-container]')
+const form = document.querySelector('#home_form')
+const firstInput = document.querySelector('#first_name')
 
 window.addEventListener('load', () => {
   const openingMessage = '👋 Hello, world! This is a Youlead challenge for a Frontend Developer position.'
@@ -9,14 +11,12 @@ window.addEventListener('load', () => {
 })
 
 ctaButton.addEventListener('click', () => {
-  const form = document.querySelector('#home_form')
-  const firstInput = document.querySelector('#first_name')
-  form.classList.add('-highlight')
+  formContainer.classList.add('-highlight')
   firstInput.focus()
 })
 
 form.addEventListener('focusout', (e) => {
   if (!e.currentTarget.contains(e.relatedTarget)) {
-    form.classList.remove('-highlight')
+    formContainer.classList.remove('-highlight')
   }
 })
