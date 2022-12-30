290307f5671b6ba5a1faf5cf1bc0316b75bdf866
Caio Viotti <caio.viotti@buser.com.br>
2022-12-30 03:08:51 -0300

    Scroll to form when clicking on the end CTA

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,21 @@
 import openToast from './app-toast'
 
+const form = document.querySelector('#home_form')
+const ctaButton = document.querySelector('[data-end-cta]')
+
 window.addEventListener('load', () => {
   const openingMessage = '👋 Hello, world! This is a Youlead challenge for a Frontend Developer position.'
   openToast(openingMessage)
+})
+
+ctaButton.addEventListener('click', () => {
+  const form = document.querySelector('#home_form')
+  form.focus()
+  form.classList.add('-highlight')
+})
+
+form.addEventListener('blur', (e) => {
+  if (!e.currentTarget.contains(e.relatedTarget)) {
+    form.classList.remove('-highlight')
+  }
 })
