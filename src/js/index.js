6117f761468a8f90a899216b4369b0297bbe0d27
Caio Viotti <caio.viotti@buser.com.br>
2022-12-30 19:28:46 -0300

    Add a concept green theme by query string
    Not exactly practical, but it fits the bill better than cloning the project
    just for custom colors.

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,12 @@ const formContainer = document.querySelector('[data-form-container]')
 const form = document.querySelector('#home_form')
 const firstInput = document.querySelector('#first_name')
 
+const url = new URL(window.location.href);
+
+if (url.searchParams.get('theme') === 'green') {
+  document.documentElement.setAttribute('data-theme', 'green')
+}
+
 window.addEventListener('load', () => {
   const openingMessage = '👋 Hello, world! This is a Youlead challenge for a Frontend Developer position.'
   openToast(openingMessage)
